6b9ef89472c90c80271be69e3380e6a7a7313017
Anurag Gorantla <anuraggorantla18@gmail.com>
2026-06-09 13:08:23 +0530

    added authorization so only authorized users can edit posts

--- main.py
+++ main.py
@@ -101,7 +101,13 @@ async def register_page(request: Request):
     )
 
 
-
+@app.get("/account", include_in_schema=False)
+async def account_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "account.html",
+        {"title": "Account"},
+    )
 
 
 
